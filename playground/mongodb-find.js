048b9c799a65a83ebd1710df5274c0ba28b00a2a
Mario Serrano <maseflores79@gmail.com>
2019-01-17 13:02:52 -0800

    ejercicios de find

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,6 +8,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
     console.log('Connected to MongoDB Server');
     const db = client.db('TodoApp');
 
+    // cargando todos
     db.collection('Todos').find().toArray().then((docs)=>{
         console.log('Todos Found');
         console.log(JSON.stringify(docs, undefined, 2));
@@ -15,6 +16,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
         console.log('Unable to fetch todos', err);
     });
 
+    // mostrando solo 1 buscando por _id
     db.collection('Todos').find({
         _id: new ObjectID('5c3fe3f21384e12d70a321eb')
     })
@@ -24,6 +26,14 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
     }, (err) => {
         console.log('Unable to fetch todos', err);
     });
+
+    // cargando el numero de total de registros
+    db.collection('Todos').find().count().then((count)=>{
+        console.log(`Todos count: ${count}`);
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
 
    //  client.close();
 });
