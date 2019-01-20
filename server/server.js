87d96cbd7b9f80816c57efc1610ebd33f3f5a72c
Mario Serrano <maseflores79@gmail.com>
2019-01-20 15:12:43 -0800

    Validators, Types y Defaults

--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true })
 
 // Creating the [Todo] model, mongoose pluralizes the database name in mongodb ([Todos])
 var Todo = mongoose.model('Todo', {
-    text: { type: String },
-    completed: { type: Boolean },
-    completedAt: { type: Number }
+    text: { type: String, required: true, minlength: 1, trim: true },
+    completed: { type: Boolean, default: false },
+    completedAt: { type: Number, default: null }
 });
 
 var newTodo = new Todo({
-    text: 'Do Yoga'
+    text: 'test'
 }); 
 
 newTodo.save().then( (doc) => {
@@ -19,3 +19,35 @@ newTodo.save().then( (doc) => {
 }, (e)=>{ 
     console.log('Unable to save TODO.');
 });
+
+var otherTodo = new Todo({
+    text: 'test 2'
+});
+
+otherTodo.save().then( (doc) => {
+    console.log( JSON.stringify(doc, undefined, 2));
+}, (e)=>{ 
+    console.log('Unable to save TODO.');
+});
+
+
+// User
+// email - require it - trim it - set type - set min length of 1
+var User = mongoose.model('User', {
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1
+    }
+  });
+  
+  var user = new User({
+    email: 'mario@example.com   '
+  });
+  
+  user.save().then((doc) => {
+    console.log('User saved', doc);
+  }, (e) => {
+    console.log('Unable to save user', e);
+  });
