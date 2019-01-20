22e9c9a5ce97e338d58390a67e9b927e15510d42
Mario Serrano <maseflores79@gmail.com>
2019-01-20 15:26:46 -0800

    Refactorizando db, modelos y construyendo API Server

---
+++ server/models/todo.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+// Creating the [Todo] model, mongoose pluralizes the database name in mongodb ([Todos])
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
+});
+
+module.exports = {Todo};
--- server/server.js
+++ server/server.js
@@ -1,53 +1,55 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-// Creating the [Todo] model, mongoose pluralizes the database name in mongodb ([Todos])
-var Todo = mongoose.model('Todo', {
-    text: { type: String, required: true, minlength: 1, trim: true },
-    completed: { type: Boolean, default: false },
-    completedAt: { type: Number, default: null }
-});
+var app = express();
 
-var newTodo = new Todo({
-    text: 'test'
-}); 
+app.use(bodyParser.json());
 
-newTodo.save().then( (doc) => {
-    console.log('Saved todo', doc);
-}, (e)=>{ 
-    console.log('Unable to save TODO.');
-});
-
-var otherTodo = new Todo({
-    text: 'test 2'
-});
-
-otherTodo.save().then( (doc) => {
-    console.log( JSON.stringify(doc, undefined, 2));
-}, (e)=>{ 
-    console.log('Unable to save TODO.');
-});
-
-
-// User
-// email - require it - trim it - set type - set min length of 1
-var User = mongoose.model('User', {
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 1
-    }
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
   });
-  
-  var user = new User({
-    email: 'mario@example.com   '
-  });
-  
-  user.save().then((doc) => {
-    console.log('User saved', doc);
+
+  todo.save().then((doc) => {
+    res.send(doc);
   }, (e) => {
-    console.log('Unable to save user', e);
+    res.status(400).send(e);
   });
+});
+
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
+});
+
+module.exports = {app};
