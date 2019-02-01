66518dc958c1aa1a4c5dfccb32304a20e3d10683
makadown <maseflores79@gmail.com>
2019-01-31 20:44:42 -0800

    quitando node modules

--- server/models/user.js
+++ server/models/user.js
@@ -45,7 +45,8 @@ UserSchema.methods.generateAuthToken = function () {
   var access = 'auth';
   var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
-  user.tokens.push({access, token});
+  // user.tokens.push({access, token});
+  user.tokens = user.tokens.concat([{access, token}]);
 
   return user.save().then(() => {
     return token;
