f2947bab47bbf8a4a556614a74b2c24e04734011
makadown <maseflores79@gmail.com>
2019-01-31 20:42:08 -0800

    iniciando mi version

--- server/config/config.js
+++ server/config/config.js
@@ -1,16 +1,10 @@
-const env = process.env.NODE_ENV || 'development';
-const testString = 'test';
-const devString = 'development';
+var env = process.env.NODE_ENV || 'development';
 
-// console.log(`Comparing [${env}] with [${testString}] and [${devString}]`);
-if (env.trim() === devString) {
-  process.env.PORT = 3000;
-  console.log('Setting development environment');
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env.trim() === testString) {
-  console.log('Setting test environment');
-  process.env.PORT = 3001;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
-
-// console.log('Mongo DB URI set to ', process.env.MONGODB_URI );
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,6 @@
-require('../config/config');
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// console.log('Connecting to ' , process.env.MONGODB_URI);
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
